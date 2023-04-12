e53c5fb6281b2e403157f7d74fe96ea01e435d6b
Brandt Carlson <brandt.carlson11@gmail.com>
2023-04-11 20:25:37 -0500

    day 2

--- src/App.js
+++ src/App.js
@@ -4,16 +4,22 @@ import { Routes, Route } from 'react-router-dom';
 import Nav from './components/Nav';
 import Home from './views/Home';
 import Shop from './views/Shop';
+import { useState } from 'react';
+
+// a quick note on props vs state:
+// props- is an object of arbitary inputs a React component accepts as the first arguement
+// State- is data that changes over the lifetime of a specific instance of a react component
 
 function App() {
+  const [teachers, setTeachers] = useState(['Brendan', 'Brandt', 'Aubrey', 'Rachel', 'Mikelyn']);
 
   return (
 
     <div className="App">
-      <Nav />
+      <Nav teachers={teachers}/>
       <Routes>
         {/*  any 'page' of our app can be defined as a route here */}
-        <Route children path='/' element={<Home />} />
+        <Route children path='/' element={<Home teachers={teachers} setTeachers={setTeachers}/>} />
         <Route children path='/shop' element={<Shop />} />
 
       </Routes>
