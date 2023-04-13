b93d2eff99c48b2c23a5f73a8aef8cf852c0890a
Brandt Carlson <brandt.carlson11@gmail.com>
2023-04-13 18:50:39 -0500

    cart done day4

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,18 +1,24 @@
 import { Link } from "react-router-dom";
-import { useEffect } from "react";
+import { useContext, useEffect } from "react";
+import '../css/nav.css';
+import { DataContext } from "../context/DataProvider";
 
 const Nav = props => {
 
-    useEffect(() => {console.log('The Nav component has rendered (or re-rendered)')});    
+    useEffect(() => {console.log('The Nav component has rendered (or re-rendered)')});   
+    const {cart} = useContext(DataContext); 
     return (
         <div>
-            <nav className="navbar navbar-expand navbar-light bg-secondary">
-                <div className="nav navbar-nav">
+            <nav className="navbar navbar-expand-sm navbar-light bg-secondary">
+                <div className="container-fluid">
                     <Link className="nav-item nav-link active" to='/'>Home</Link>
                     <Link className="nav-item nav-link active" to='/shop'>Shop</Link>
                     <Link className="nav-item nav-link active" to='/cart'>Cart</Link>
-                </div>
-                <span>{props.teachers[Math.floor(Math.random()*props.teachers.length)]}</span>
+                { cart.size === 0 ?
+                <span id="r-span"><Link className="nav-item nav-link active" to='/shop'><i class="fa-solid fa-cart-shopping"></i></Link></span> :
+                <span id="r-span"><Link className="nav-item nav-link active" to='/cart'>{cart.size} - ${cart.total.toFixed(2)} <i class="fa-solid fa-cart-shopping"></i></Link></span>
+                }
+            </div>
             </nav>
         </div>
     );
--- src/views/Cart.js
+++ src/views/Cart.js
@@ -3,6 +3,7 @@ import { DataContext } from "../context/DataProvider";
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
 import { Button } from "react-bootstrap";
+import '../css/customCart.css';
 
 const Cart = () => {
     /*
@@ -14,6 +15,39 @@ const Cart = () => {
     */
     const { cart, setCart } = useContext(DataContext);
 
+    const clearCart = () => {
+        setCart({size:0, total:0, products: {}});
+        console.log(cart)
+    }
+
+    const increaseQuantity = id => {
+        // create a copy
+        let copyCart = {...cart};
+        // modify the copy
+        copyCart.size++;
+        copyCart.total += copyCart.products[id].data.price;
+        copyCart.products[id].quantity++;
+        //set the state
+        setCart(copyCart);
+    }
+
+    const decreaseQuantity = id => {
+        let copyCart = {...cart};
+        copyCart.size--;
+        copyCart.total -= copyCart.products[id].data.price;
+        copyCart.products[id].quantity > 1 ?
+        copyCart.products[id].quantity-- :
+        delete copyCart.products[id];
+        setCart(copyCart)
+    }
+    const removeItem = id => {
+        let copyCart = {...cart};
+        copyCart.size -= copyCart.products[id].quantity;
+        copyCart.total -= copyCart.products[id].quantity*copyCart.products[id].data.price;
+        delete copyCart.products[id];
+        setCart(copyCart)
+    }
+
     return (
 
         <Card style={{ width: '18rem' }}>
@@ -24,19 +58,22 @@ const Cart = () => {
             {Object.values(cart.products).map((product, index) => {
                 console.log(product);
                 return <ListGroup.Item key={index}>
-                    <Card.Img variant="top" src={product.data.img_url} />
+                    <Card.Img variant="top" src={product.data.img_url} id="p-img"/>
                     <h3>{product.data.name}</h3>
                     <h5>{product.data.make} {product.data.model}</h5>
-                    <h5>Price: {product.data.price}</h5>
-                    <Button variant="warning"><b> - 1 </b></Button>
-                    <span >{product.quantity}</span>
-                    <Button variant="success"><b> + 1 </b></Button>
+                    <h6>Price: {product.data.price}</h6>
+                    <Button variant="secondary" id="dec-btn" onClick={() => {decreaseQuantity(product.data.id)}}><b> - 1 </b></Button>
+                    <span id="q-span">{product.quantity}</span>
+                    <Button variant="success" id="inc-btn" onClick={() => {increaseQuantity(product.data.id)}}><b> + 1 </b></Button>
+                    <br></br>
+                    <Button variant="warning" id="r-item" onClick={() => {removeItem(product.data.id)}}>remove this item</Button>
                 </ListGroup.Item>
             })}
             
             </ListGroup>
             <Card.Body>
                 <Button variant="primary">Checkout</Button>
+                <Button variant="danger" onClick={clearCart}>Clear Cart</Button>
             </Card.Body>
         </Card>
     )
