7a42d35daa76934f9c52579849f7fdf2d35fc685
Brandt Carlson <brandt.carlson11@gmail.com>
2023-04-25 20:07:00 -0500

    auth done

--- src/components/AdminSignin.js
+++ src/components/AdminSignin.js
@@ -1,9 +1,9 @@
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
-export default function AdminSignin() {
+export default function AdminSignin(props) {
 
-    const handleSubmit = async (e) => {
+    const handleSignin = async (e) => {
         e.preventDefault();
         const res = await fetch('http://127.0.0.1:5000/api/signin', {
             method: 'POST',
@@ -17,12 +17,27 @@ export default function AdminSignin() {
         });
         const data = await res.json();
         console.log(data);
+        props.setAdmin({'in': true, 'token': data.data.token})
     }
+    const handleBasicAuth = async (e) => {
+      e.preventDefault();
+      console.log(e);
+      console.log(props.admin['token'])
+      const res = await fetch('http://127.0.0.1:5000/api/token', {
+        method : 'POST',
+        headers: {
+          'Authorization': `Bearer ${btoa(e.target[0].value + ':' + e.target[1].value)}`,
+          'Content-Type' : 'application/json'
+      },
+    });
+        const data = await res.json();
+        console.log(data);
+  }
 
   return (
     <div className="container justify-content-center">
         <h3>Admin Sign In</h3>
-    <Form className="mt-auto mt-5" onSubmit={(e) => {handleSubmit(e)}}>
+    <Form className="mt-auto mt-5" onSubmit={(e) => {handleBasicAuth(e)}}>
       <Form.Group className="mb-3">
         <Form.Label>Username</Form.Label>
         <Form.Control type="text" placeholder="Enter Username" />
